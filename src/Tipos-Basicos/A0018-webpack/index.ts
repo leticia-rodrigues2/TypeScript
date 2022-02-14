cf70d9d1ec25522eaa28b23d149467ebe0c823b3
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2022-02-13 23:51:16 -0300

    webpack

--- src/Tipos-Basicos/A0018-webpack/index.ts
+++ src/Tipos-Basicos/A0018-webpack/index.ts
@@ -1,7 +1,8 @@
-console.log("Olá muuundo!");
+import funcao from "./mod";
+funcao();
 
 /* CONFIG WEBPACK
 1 npm i ts-loader webpack-cli--save-dev
 2 npx webpack
-3
+3 npx webpack -w (SEMPRE RODA PARA FICA SENDO OLHANDO O QUE ESTÁ SENDO FEITO)
 4 */
---
+++ src/Tipos-Basicos/A0018-webpack/mod.ts
@@ -0,0 +1,3 @@
+export default (): void => {
+  console.log("****** nSou o módulo **********");
+};
