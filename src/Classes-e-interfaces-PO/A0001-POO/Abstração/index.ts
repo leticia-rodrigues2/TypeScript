bedb46b7ac43eda705ff430778fbbc7d4870adcc
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2022-02-16 21:04:36 -0300

    Classes-e-interfaces-PO

---
+++ src/Classes-e-interfaces-PO/A0001-POO/Abstração/index.ts
@@ -0,0 +1,9 @@
+export class Pessoa {
+  private nome : string;
+  private sobrenome : string;
+
+  constructor(nome : string , sobrenome: string){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+}
