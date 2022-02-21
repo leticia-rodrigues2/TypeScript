4eb4c606d71ac19b1277db8511d682f98794b710
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2022-02-21 08:46:09 -0300

    Polimorfismos

---
+++ src/Classes-e-interfaces-PO/A0001-POO/Plimorfismo/index.ts
@@ -0,0 +1,15 @@
+import {Animal, Cat  , Dog} from "../Herança";
+
+
+// class Animalsounds {
+//   public playSound (animal : Animal) : void {
+//     animal.makeNoise();
+//   }
+// }
+
+// const dog = new Dog ('Tina') ;
+// const cat = new Cat ('Suzy');
+
+// const Animalsounds = new Animalsounds();
+// Animalsounds.playSound(dog) // Tina faz auaua
+// Animalsounds.playSound(cat) // Suzy faz miau
